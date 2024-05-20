2e6f7864ff0d4f84e7fb2fa468677e0349307f57
pratima-olixlab <olixlab.22@gmail.com>
2024-05-20 17:22:25 +0530

    .env commit

--- db.js
+++ db.js
@@ -12,51 +12,21 @@
 
 // module.exports = mongoose;
 
-
-
-
-
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// const dbUri = process.env.MONGODB_URI;
-// console.log('dbUri:::::::::::::::::::::::::::::',dbUri);
-// mongoose.connect(dbUri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-//   .then(() => {
-//     console.log('MongoDB connection succeeded');
-//   })
-//   .catch((err) => {
-//     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
-//   });
-
-// require('./models/user.model'); // This line imports the Mongoose model for the 'User' schema.
-
-// module.exports = mongoose;
-
-
-
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
-// Load environment variables from the appropriate .env file
-const env = process.env.NODE_ENV || 'development';
-dotenv.config({ path: `.env.${env}` });
-
-console.log('env:::::::::::::::::',env, process.env.MONGODB_URI, process.env.PORT);
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const dbUri = process.env.MONGODB_URI;
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.then(() => {
-  console.log('MongoDB connection succeeded');
-})
-.catch((err) => {
-  console.log('Error in DB connection: ' + JSON.stringify(err, undefined, 2));
-});
+  .then(() => {
+    console.log('MongoDB connection succeeded');
+  })
+  .catch((err) => {
+    console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+  });
 
-require('./models/user.model'); // Import the Mongoose model for the 'User' schema
+require('./models/user.model');
 
 module.exports = mongoose;
