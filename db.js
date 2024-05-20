15af40dfd969e2df45a80587d165228e6cd40314
pratima-olixlab <olixlab.22@gmail.com>
2024-05-20 18:21:03 +0530

    .db.js commit

--- db.js
+++ db.js
@@ -1,27 +1,6 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb+srv://olixlab22:OlixLab1@cluster0.w9ceppl.mongodb.net/mern-stack-react')
-//   .then(() => {
-//     console.log('MongoDB connection succeeded');
-//   })
-//   .catch((err) => {
-//     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
-//   });
-
-// require('./models/user.model'); //This line imports the Mongoose model for the 'User' schema.
-
-// module.exports = mongoose;
-
 const mongoose = require('mongoose');
-require('dotenv').config();
-require('./config/configs');
 
-const dbUri = process.env.MONGODB_URI;
-
-mongoose.connect(dbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect('mongodb+srv://olixlab22:OlixLab1@cluster0.w9ceppl.mongodb.net/mern-stack-react')
   .then(() => {
     console.log('MongoDB connection succeeded');
   })
@@ -29,6 +8,27 @@ mongoose.connect(dbUri, {
     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
   });
 
-require('./models/user.model'); // This line imports the Mongoose model for the 'User' schema.
+require('./models/user.model'); //This line imports the Mongoose model for the 'User' schema.
 
 module.exports = mongoose;
+
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// require('./config/configs');
+
+// const dbUri = process.env.MONGODB_URI;
+
+// mongoose.connect(dbUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//   .then(() => {
+//     console.log('MongoDB connection succeeded');
+//   })
+//   .catch((err) => {
+//     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+//   });
+
+// require('./models/user.model'); // This line imports the Mongoose model for the 'User' schema.
+
+// module.exports = mongoose;
