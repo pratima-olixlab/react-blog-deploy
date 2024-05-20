ce56a4477e4633c6a97dea81088742d17480680a
pratima-olixlab <olixlab.22@gmail.com>
2024-05-20 15:25:38 +0530

    database commit

--- config/configs.js
+++ config/configs.js
@@ -1,6 +1,8 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 
 var config = require('./config.json');
 var envConfig = config[env];
 
-Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key];
+});
--- db.js
+++ db.js
@@ -1,6 +1,27 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://olixlab22:OlixLab1@cluster0.w9ceppl.mongodb.net/mern-stack-react')
+// mongoose.connect('mongodb+srv://olixlab22:OlixLab1@cluster0.w9ceppl.mongodb.net/mern-stack-react')
+//   .then(() => {
+//     console.log('MongoDB connection succeeded');
+//   })
+//   .catch((err) => {
+//     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+//   });
+
+// require('./models/user.model'); //This line imports the Mongoose model for the 'User' schema.
+
+// module.exports = mongoose;
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+require('./config/configs');
+
+const dbUri = process.env.MONGODB_URI;
+
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
   .then(() => {
     console.log('MongoDB connection succeeded');
   })
@@ -8,6 +29,6 @@ mongoose.connect('mongodb+srv://olixlab22:OlixLab1@cluster0.w9ceppl.mongodb.net/
     console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
   });
 
-require('./models/user.model'); //This line imports the Mongoose model for the 'User' schema.
+require('./models/user.model'); // This line imports the Mongoose model for the 'User' schema.
 
 module.exports = mongoose;
